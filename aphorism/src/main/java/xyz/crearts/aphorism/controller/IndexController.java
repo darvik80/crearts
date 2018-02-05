13ebc2dd16b27f44f1879238a12e997ab5a0fbdf
Ivan Kishchenko <ivan.kishchenko@lazada.com>
2018-02-05 18:55:29 +0800

    react.js

--- aphorism/src/main/java/xyz/crearts/aphorism/controller/IndexController.java
+++ aphorism/src/main/java/xyz/crearts/aphorism/controller/IndexController.java
@@ -1,20 +1,9 @@
 package xyz.crearts.aphorism.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import xyz.crearts.aphorism.model.EnvelopmentValue;
-import xyz.crearts.aphorism.model.TinyMCEForm;
 import xyz.crearts.aphorism.repository.AphorismRepository;
-import xyz.crearts.aphorism.repository.EnvelopmentValueRepository;
-import xyz.crearts.aphorism.services.EnvValuesService;
-
-import javax.jws.WebParam;
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author ivan.kishchenko
---
+++ aphorism/src/main/java/xyz/crearts/aphorism/controller/ReactController.java
@@ -0,0 +1,18 @@
+package xyz.crearts.aphorism.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author ivan.kishchenko
+ */
+@Controller
+@RequestMapping(value = "/react")
+public class ReactController {
+    @GetMapping({"/"})
+    public String reactJsAction(Model model) {
+        return "react";
+    }
+}
